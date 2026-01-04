e22cc0a8434c37c0dcc4d8dda2459a07cfee5d7b
Irfan <m.irfanwatoo@gmail.com>
2026-01-04 00:26:19 +0000

    Deploy: update from boilerplate

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2628914305"
+    serviceWorkerVersion: "1108916337"
   }
 });
